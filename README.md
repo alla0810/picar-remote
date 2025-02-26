f1439a5f9e935c4d7573e902d5f0749c0f660285
alla0810 <kyosook.shin@gmail.com>
2025-02-25 18:54:10 -0600

    initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # picar-remote
+
+npm init -y
+npm install electron
+npm start
+npm clean
+npm start
---
+++ bt_server.py
@@ -0,0 +1,23 @@
+import bluetooth
+
+hostMACAddress = "D8:3A:DD:6D:E5:D4" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 0
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+print("listening on port ", port)
+try:
+    client, clientInfo = s.accept()
+    while 1:   
+        print("server recv from: ", clientInfo)
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data) # Echo back to client
+except: 
+    print("Closing socket")
+    client.close()
+    s.close()
+
