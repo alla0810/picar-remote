9b6bf7813565fe301e53b8bc3b285aebb118b7c5
Jeffery Fan <jefferyfan3a@gmail.com>
2025-03-06 16:53:20 -0600

    requirements.txt as well as audo ip fetcher in wifi server

--- wifi_server.py
+++ wifi_server.py
@@ -1,9 +1,24 @@
 import socket
 from time import sleep
 from picarx import Picarx
+import netifaces as ni
 
-HOST = "192.168.1.86" # IP address of your Raspberry PI
-PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+
+# Get Pi's IP address
+def get_ip_address():
+    interfaces = ni.interfaces()
+    for interface in interfaces:
+        if interface != "lo":
+            try:
+                ip = ni.ifaddresses(interface)[ni.AF_INET][0]["addr"]
+                return ip
+            except KeyError:
+                continue
+    return "127.0.0.1"
+
+
+HOST = get_ip_address()
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 px = Picarx()
 pan_angle = 0
@@ -18,10 +33,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while 1:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
-            data = client.recv(1024)      # receive 1024 Bytes of message in binary format
+            data = client.recv(1024)  # receive 1024 Bytes of message in binary format
 
             if data != b"":
-                print(data)     
+                print(data)
 
                 text = data.decode().strip().lower()
                 print(f"Decoded data: {text}")
@@ -33,8 +48,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         tilt_angle = 60
                 elif text == "headdown":
                     print("tilt down pressed!")
-                    tilt_angle -=5
-                    if tilt_angle <-60:
+                    tilt_angle -= 5
+                    if tilt_angle < -60:
                         tilt_angle = -60
                 elif text == "headleft":
                     print("pan left pressed!")
@@ -66,23 +81,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     print("drive right pressed!")
                     px.set_dir_servo_angle(30)
                     px.forward(80)
-                        
+
                 px.set_cam_tilt_angle(tilt_angle)
                 px.set_cam_pan_angle(pan_angle)
                 sleep(0.5)
-                px.forward(0)                
+                px.forward(0)
 
-                client.sendall(data) # Echo back to client                    
+                client.sendall(data)  # Echo back to client
 
-    except: 
+    except:
         print("Closing socket")
         client.close()
-        s.close()    
+        s.close()
 
     finally:
         px.set_cam_tilt_angle(0)
         px.set_cam_pan_angle(0)
         px.set_dir_servo_angle(0)
         px.stop()
-        sleep(.2)
-            
+        sleep(0.2)
